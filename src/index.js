d60ff095fb4244a62a62cdedc24ba1a2b5f9b892
josh handelman <handelmanjos@gmail.com>
2024-05-14 18:36:48 -0400

    stuff

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const { exec } = require("child_process");
+
+async function McWater() {
+    // Create new order with McWater from McDonalds
+    const scriptPath = './requests/createDraftOrder.sh';
+    const response = await new Promise((resolve, reject) => {
+        exec(`sh ${scriptPath}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing script: ${error.message}`);
+                reject(error.message);
+            }
+            if (stderr) {
+                console.error(`Script error output: ${stderr}`);
+            }
+            if (stdout) {
+                resolve(stdout);
+            }
+        })
+    });
+    const json = JSON.parse(response);
+    console.log(json);
+    // return order uuid
+    return json.data.draftOrder.uuid;
+}
+async function Order(uuid) {
+    const scriptPath = "./requests/checkoutDraftOrder.sh";
+    const response = await new Promise((resolve, reject) => {
+        exec(`sh ${scriptPath} ${uuid}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing script: ${error.message}`)
+                reject(error.message)
+            }
+            if (stderr) {
+                console.error(`Script error output: ${stderr}`);
+            }
+            if (stdout) {
+                resolve(stdout);
+            }
+        })
+    });
+    console.log(response);
+}
+async function main() {
+    const uuid = await McWater();
+    console.log({uuid});
+    await Order(uuid);
+}
+
+
+main();
